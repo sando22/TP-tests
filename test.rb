b901263892d17db4f8ec09146e5878e5df80ae74
sando <sando@ubuntu>
2014-06-23 22:27:49 +0300

    identation fix

--- test.rb
+++ test.rb
@@ -18,84 +18,81 @@ class Tests
     end
 
     def type1
-	orig = Generator.new()
-	orig.generate_random_hex
-	
-	shift = rand(6...8)
-	
-	insert = Generator.new()
-	insert.generate_random_hex
-	
-	a = "orig | (insert << #{shift})"
+    	orig = Generator.new()
+    	orig.generate_random_hex
 
+    	shift = rand(6...8)
+
+    	insert = Generator.new()
+    	insert.generate_random_hex
+
+    	a = "orig | (insert << #{shift})"
     end
 
     def type2
     	orig = Generator.new()
-	orig.generate_random_hex
-	
-	shift = rand(6...8)
-	
-	insert = Generator.new()
-	insert.generate_random_hex
-	
-	a = orig | (insert << #{shift})
-	
-	b = orig | (insert << #{shift})
-	
-	andd = "a & b"
+    	orig.generate_random_hex
+
+    	shift = rand(6...8)
+
+    	insert = Generator.new()
+    	insert.generate_random_hex
+
+    	a = orig | (insert << #{shift})
+
+    	b = orig | (insert << #{shift})
+
+    	andd = "a & b"
     end
 
     def type3
-	i=Generator.new()
-	i.generate_random_hex
-	
-	number = rand(5..10)
-	
-	left = "i | (1<<#{i})"
-	
+    	i=Generator.new()
+    	i.generate_random_hex
+
+    	number = rand(5..10)
+
+    	left = "i | (1<<#{i})"
     end
 
     def type4
     	value1=Generator.new()
     	value1.generate_random_hex_long
-    	
-    	shift1 = rand(2..3)
-	shift2 = rand(2..3)
-	
-    	value2=Generator.new()
-	value2.generate_random_hex_long
-	
-	result = "(value1 << #{shift1}) ^ (value2 >> #{shift2})"
 
+    	shift1 = rand(2..3)
+    	shift2 = rand(2..3)
+
+        	value2=Generator.new()
+    	value2.generate_random_hex_long
+
+    	result = "(value1 << #{shift1}) ^ (value2 >> #{shift2})"
     end
 
     def type5
-	value1 = rand(100..600)
-	value2 = rand(100..600)
+    	value1 = rand(100..600)
+    	value2 = rand(100..600)
 
-	shift1 = rand(2..3)
-	shift2 = rand(2..3)
-	
-	result = "(value1 << #{shift1}) ^ (value2 >> #{shift2})"
+    	shift1 = rand(2..3)
+    	shift2 = rand(2..3)
+
+    	result = "(value1 << #{shift1}) ^ (value2 >> #{shift2})"
     end
 
     def type6
-	testValue = Generator.new()
-	testValue.generate_random_hex_long
+    	testValue = Generator.new()
+    	testValue.generate_random_hex_long
 
-	shift = rand(2..4)
-	
-	if_test = "if(testValue & (1 << #{shift}))"
+    	shift = rand(2..4)
+
+    	if_test = "if(testValue & (1 << #{shift}))"
     end
 
     def type7
-	testValue = Generator.new()
-	testValue.generate_random_hex_long
+    	testValue = Generator.new()
+    	testValue.generate_random_hex_long
 
-	shift = rand(2..4)
+    	shift = rand(2..4)
 
-	if_test = "if((result = testValue & testValue ^ testValue | (1 << #{shift})))"
+    	if_test = "if((result = testValue & testValue ^ testValue | (1 << #{shift})))"
     end
 
 end
